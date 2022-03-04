b11d1e39b0db1e36360037e561cf1d35d4252f72
Andrew McCall <amccall@qa.com>
2022-03-04 18:55:47 +0000

    Basic Structure. Window Created

--- Fortress/src/main.rs
+++ Fortress/src/main.rs
@@ -1,3 +1,44 @@
+use piston_window::*;
+use std::time::{SystemTime, Duration};
+use rand::{Rng};
+
 fn main() {
-    println!("Hello, world!");
+
+
+    let mut window: PistonWindow = WindowSettings::new("Fortress", [480, 360]).exit_on_esc(true).build().unwrap();
+
+    let mut cursor = [0.0, 0.0];
+
+    while let Some(event) = window.next() {
+
+        event.mouse_cursor(|pos| {
+            cursor = pos;
+            println!("Mouse moved '{} {}'", pos[0], pos[1]);
+        });
+        
+        if let Some(cursor) = event.cursor_args() {
+            if cursor { println!("Mouse entered"); }
+            else { println!("Mouse left"); }
+        };
+
+        if let Some(_args) = event.render_args() {
+            window.draw_2d(&event, |context, graphics, _device| {
+                clear([1.0; 4], graphics);
+
+                rectangle([0.0,0.4,0.0,1.0],
+                    [0.0, 0.0, 480.0, 360.0],
+                    context.transform,
+                    graphics);
+
+            });
+        };
+
+        
+        // if let Some(_args) = event.idle_args() {
+        // }
+
+        // if let Some(_args) = event.update_args() {
+        // }
+
+    }
 }
